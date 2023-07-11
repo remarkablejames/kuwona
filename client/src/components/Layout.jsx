fe5bb743a8ce2b58771db181e82c005f1f136516
James Niyongira <lascasasjames85@gmail.com>
2023-07-11 15:44:02 -0400

    Added navigation bar

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,7 +1,21 @@
 import {Outlet} from "react-router-dom";
+import Navbar from "./common/Navbar.jsx";
 
 function Layout(){
-    return <Outlet />
+    return (
+        <>
+            <div className="min-h-full">
+
+                <Navbar />
+                <main>
+                    <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-2">
+                        <Outlet />
+                    </div>
+                </main>
+            {/*    Footer goes here*/}
+            </div>
+        </>);
+
 }
 
 export default Layout;
---
+++ client/src/components/common/Navbar.jsx
@@ -0,0 +1,75 @@
+import {Link} from "react-router-dom";
+
+function Navbar(){
+    return(
+        <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0 dark:bg-gray-800 dark:border-gray-700">
+            <nav
+                className="relative max-w-7xl w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
+                aria-label="Global"
+            >
+                <div className="flex items-center justify-between">
+                    <Link
+                        className="flex-none text-xl font-semibold dark:text-white"
+                        to="/"
+                        aria-label="Brand"
+                    >
+                        ideaForum
+                    </Link>
+                    <div className="sm:hidden">
+                        <button
+                            type="button"
+                            className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
+                            data-hs-collapse="#navbar-collapse-with-animation"
+                            aria-controls="navbar-collapse-with-animation"
+                            aria-label="Toggle navigation"
+                        >
+                            <svg
+                                className="hs-collapse-open:hidden w-4 h-4"
+                                width={16}
+                                height={16}
+                                fill="currentColor"
+                                viewBox="0 0 16 16"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+                                />
+                            </svg>
+                            <svg
+                                className="hs-collapse-open:block hidden w-4 h-4"
+                                width={16}
+                                height={16}
+                                fill="currentColor"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                <div
+                    id="navbar-collapse-with-animation"
+                    className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
+                >
+                    <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7">
+
+
+
+                        <a
+                            className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600   sm:my-6 sm:pl-6  dark:text-gray-400 dark:hover:text-blue-500"
+                            href="#"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                            </svg>
+                            Log in
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+    )
+}
+
+export default Navbar
