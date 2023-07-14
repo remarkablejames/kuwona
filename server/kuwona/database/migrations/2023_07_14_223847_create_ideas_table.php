641a74f6c7c2078a3a672ff50befc752c9b287c8
kennykobain <kenenduba@gmail.com>
2023-07-14 19:25:04 -0400

    migrate table create commit

--- server/kuwona/database/migrations/2023_07_14_223847_create_ideas_table.php
+++ server/kuwona/database/migrations/2023_07_14_223847_create_ideas_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('ideas', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('description')->nullable();
+            $table->decimal('rating', 2, 1)->default(0);
             $table->timestamps();
         });
     }
