f929ffaa4fd5ff5d6bd664a8c1e222246c2db23b
kennykobain <kenenduba@gmail.com>
2023-07-15 14:00:26 -0400

    model and controller create commit

--- server/kuwona/app/Http/Controllers/IdeaController.php
+++ server/kuwona/app/Http/Controllers/IdeaController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class IdeaController extends Controller
 {
-    //
+    public function index()
+    {
+        $ideas = Idea::all();
+        return response()->json($ideas, 200);
+    }
+
+    public function show($id)
+    {
+        $idea = Idea::find($id);
+        return response()->json($idea, 200);
+    }
+
+    // create a new idea
+    public function store()
+    {
+        $idea = new Idea();
+        $idea->title = request('title');
+        $idea->slug = request('slug');
+        $idea->description = request('description');
+        $idea->rating = request('rating');
+        $idea->save();
+
+        return response()->json($idea, 201);
+    }
+
+    // update an existing idea
+    public function update($id)
+    {
+        $idea = Idea::find($id);
+        $idea->title = request('title');
+        $idea->slug = request('slug');
+        $idea->description = request('description');
+        $idea->rating = request('rating');
+        $idea->save();
+
+        return response()->json($idea, 200);
+    }
+
+    // delete an existing idea
+    public function destroy($id)
+    {
+        $idea = Idea::find($id);
+        $idea->delete();
+
+        return response()->json(null, 204);
+    }
 }
--- server/kuwona/routes/api.php
+++ server/kuwona/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\IdeaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/ideas', [IdeaController::class, 'index']);
+
+Route::post('/ideas', [IdeaController::class, 'store']);
+
+Route::get('/ideas/{id}', [IdeaController::class, 'show']);
+
+Route::put('/ideas/{id}', [IdeaController::class, 'update']);
+
+Route::delete('/ideas/{id}', [IdeaController::class, 'destroy']);
+
