85870391d653bcccff751ddff0e6bf6d74171c71
James Niyongira <lascasasjames85@gmail.com>
2023-07-17 12:40:35 -0400

    added Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import Layout from "./components/Layout.jsx";
 import Home from "./components/Home.jsx";
 import PageNotFound from "./components/PageNotFound.jsx";
 import IdeaList from "./features/ideas/IdeaList.jsx";
+import Login from "./features/auth/Login.jsx";
 
 function App(){
     return(
         <Routes>
+            <Route path="login" element={<Login/>}/>
             <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="feed" element={<IdeaList/>} />
