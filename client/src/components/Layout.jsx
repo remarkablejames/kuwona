c3ba28435f795f9228016017c2565574169591cd
James Niyongira <lascasasjames85@gmail.com>
2023-07-17 10:01:12 -0400

    added home and layout components

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -8,7 +8,7 @@ function Layout(){
 
                 <Navbar />
                 <main>
-                    <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-2">
+                    <div className="w-full">
                         <Outlet />
                     </div>
                 </main>
