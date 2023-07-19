bea653ff64191c010dc0f5301046a9b355fa6a7e
James Niyongira <lascasasjames85@gmail.com>
2023-07-18 21:34:42 -0400

    fixed Design bugs

--- client/src/features/ideas/CreatePost.jsx
+++ client/src/features/ideas/CreatePost.jsx
@@ -21,11 +21,24 @@ function CreatePost(){
                             <div className="bg-white pl-2   w-full">
                                 <h3 className="text-xl font-semibold mb-4">Post new idea</h3>
                                 <form className="mb-4">
-                        <textarea
-                            className="w-full rounded-md border p-2 mb-2 resize-none"
-                            placeholder="Write your idea here..."
-                            rows="3"
-                        />
+                                    <div className="my-2">
+                                    <label>Idea title</label>
+                                    <input
+                                        name="icon"
+                                        type="text"
+                                        className="block w-1/2 px-4 py-2 text-sm font-normal text-gray-800 placeholder-gray-400 placeholder:italic bg-white border border-gray-300 rounded-md font-spline focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-600/50 disabled:opacity-50"
+                                        placeholder="Idea title goes here"
+                                        required=""
+                                    />
+                                    </div>
+                                    <div className="mb-4">
+                                    <label>Content</label>
+                                    <textarea
+                                        className="w-full rounded-md border p-2 mb-2 resize-none placeholder:italic"
+                                        placeholder="Write your idea here..."
+                                        rows="3"
+                                    />
+                                    </div>
                                     <button
                                         type="submit"
                                         className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
