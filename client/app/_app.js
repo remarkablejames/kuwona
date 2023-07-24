4f1541e57e7da3ebbffda3cced157fc9cccdce6b
James Niyongira <lascasasjames85@gmail.com>
2023-07-24 17:48:15 -0400

    migrated to Nextjs 13

---
+++ client/app/_app.js
@@ -0,0 +1,6 @@
+import {useEffect} from "react";
+
+'use client;'
+useEffect(() => {
+    import('preline')
+}, [])
---
+++ client/app/feed/page.jsx
@@ -0,0 +1,8 @@
+import IdeaList from './components/IdeaList.jsx';
+export default function FeedPage() {
+    return (
+        <div className="h-fulln min-h-screen">
+            <IdeaList/>
+        </div>
+    )
+}
